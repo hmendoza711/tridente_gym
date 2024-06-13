d4595895d6c065e2735a765d0f73e4d4fa379f2e
Hector Mendoza <hectormaximendoza@gmail.com>
2024-06-13 16:25:17 -0300

    Inicio tpi

--- tridente_gym/src/App.jsx
+++ tridente_gym/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
       <div>
          TRIDENTE_GYM
       </div>
-
     </>
   )
 }
--- tridente_gym/tailwind.config.js
+++ tridente_gym/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00ff00",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+        dark: "#111111",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
