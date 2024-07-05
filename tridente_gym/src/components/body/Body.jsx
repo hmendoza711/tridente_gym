f780c3108ed9acf8b453cc99d2728e720acea8e0
Rodrimache <rodri.rs.93@gmail.com>
2024-07-05 10:21:48 -0300

    Body y profesores

--- tridente_gym/src/components/body/Body.jsx
+++ tridente_gym/src/components/body/Body.jsx
@@ -1,14 +1,8 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import video from '../../../public/video.mp4';
-import { AuthenticationContext } from '../../services/authenticationContext/AuthenticationContext';
 
 const Body = () => {
-  const { isAdmin, isProfe, user } = useContext(AuthenticationContext); //Si estamos loggeados no hay boton
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/Register");
-  }
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="relative">
@@ -20,14 +14,12 @@ const Body = () => {
         <h1 className="text-4xl md:text-5xl font-bold mb-3 hover:text-primary">
           BIENVENIDO A TRIDENTE GYM
         </h1>
-        {!isAdmin && !isProfe && !user && (
-          <button
-            className="bg-green-600 hover:text-primary text-white py-2 px-4 mt-4 rounded-md"
-            onClick={handleClick}
-          >
-            ÚNETE A NUESTRO EQUIPO
-          </button>
-        )}
+        <button
+          className="bg-green-600 hover:text-primary text-white py-2 px-4 mt-4 rounded-md"
+          onClick={() => setShowText(!showText)}
+        >
+          ÚNETE A NUESTRO EQUIPO
+        </button>
       </div>
     </div>
   );
